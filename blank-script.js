0d1adbdd9c54c265e3d0ce103c2d394fb1a7624f
shinzenkoru <pongpat1540@gmail.com>
2015-08-29 21:04:35 +0700

    Blank Template with Toolbox

---
+++ blank-script.js
@@ -0,0 +1,40 @@
+function determineScreenSize(){
+ 	var height = window.innerHeight;
+ 	document.getElementById("content").style.height = height+"px";
+ 	document.getElementById("toolbox").style.height = height+"px";
+}
+
+function showToolbox() {
+	var toolbox = document.getElementById("toolbox");
+
+	if (toolbox.getBoundingClientRect().left==0) {
+		$("#toolbox").animate({
+			left: "-285px"
+		},200);
+
+		$("body").animate({
+			left: "0px"
+		},200);
+
+	} else {
+		$("#toolbox").animate({
+			left: "0px"
+		},200);
+
+		$("body").animate({
+			left: "285px"
+		},200);
+	}
+
+}
+
+function hideToolbox() {
+	var toolbox = document.getElementById("toolbox");
+	$("#toolbox").animate({
+		left: "-285px"
+	},200);
+
+	$("body").animate({
+		left: "0px"
+	},200);
+}
